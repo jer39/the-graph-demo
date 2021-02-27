2571ff2b3995a212e8a23d05898a59e5f1da3e49
Eric Cheung <ericcheung931@gmail.com>
2021-02-27 21:42:54 +0800

    initial commit

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export enum LinearFinanceAssetType {
+  ADA = "lADA",
+  BNB = "lBNB",
+  BTC = "lBTC",
+  DOT = "lDOT",
+  ETH = "lETH",
+  LINK = "lLINK",
+  TRX = "lTRX",
+  XAG = "lXAG",
+  XAU = "lXAU",
+  XLM = "lXLM",
+  YFI = "lYFI"
+}
+
+export type TokenVolumes = {
+  volume: string;
+};
+
+export type TransferItem = {
+  id: string;
+  from: string;
+  to: string;
+  value: string;
+  timestamp: number;
+  block: number;
+}
+
+export type TransferObj = {
+  data: {
+    transfers: TransferItem[];
+  }
+}
